6960cd8223c1fd7cfe4fb8b45cb2c1a65dd7206b
Thehackitect <73643858+TheHackitect@users.noreply.github.com>
2024-11-06 10:19:06 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
 
-BOT_TOKEN = "6929527011:AAHHlznmmjBeU9DmC97Dp6vTB8700a5PhgQ"
+BOT_TOKEN = "xxxxxxxxxxxxxxxxxxxxxxxxxxx"
 ADMIN_ID = 1233125771  # Replace with the actual admin Telegram ID
 DATABASE_URL = "sqlite:///my_bot.db"
